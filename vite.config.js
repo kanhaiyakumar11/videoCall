7c83ad7ff262144bda33c0c6bcd747223515ea83
Kanhaiya Kumar <115523368+kanhaiyakumar11@users.noreply.github.com>
2025-03-25 23:05:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // 1000KB limit set kar raha hai
+  }
+});
